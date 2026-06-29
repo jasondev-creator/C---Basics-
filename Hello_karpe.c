bec8e5461102e25dc1d53406dc85b7cdfeb77ddb
jasondev-creator <jasonstudy610@gmail.com>
2026-06-29 21:21:04 +0530

    My first C programs

---
+++ Hello_karpe.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+   printf("Hello Karpe!");
+   return 0;
+   }
---
+++ char_int_float.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main () {
+char name [ ] = "jason";
+printf("%s\n",name);
+int age = 18;
+printf("my age is %d\n",age);
+float height = 6.2;
+printf("%f\n",height);
+return 0;
+}
