d2cbffe8a51d2d3413b414438d238b26dcf10ad1
jasondev-creator <jasonstudy610@gmail.com>
2026-07-02 20:37:52 +0530

    Add Forward_And_Backward_Loops.c and update Operators.c

--- Operators.c
+++ Operators.c
@@ -2,9 +2,14 @@
 int main() {
 int a = 20;
 int b = 10;
+// Compare
 printf("%d\n", a == b);
+// Not Equal to
 printf("%d\n", a != b);
+// Greater thna or Equal to
 printf("%d\n", a >= b);
+// Lesser than or Equal to
 printf("%d\n", a <= b);
+// Output
 return 0;
 }
